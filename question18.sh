389702ed1925fc1bfea41025915cf74a5add7268
Sshhiivam <129131233+Sshhiivam@users.noreply.github.com>
2023-06-12 15:45:46 +0930

    Update question18.sh

--- question18.sh
+++ question18.sh
@@ -1,22 +1,15 @@
 git clone https://github.com/ian-knight-uofa/git-practice-04.git
-cd git-practice-04
 
-# Identify all branches
+cd git-practice-04
 git branch -a
 
-# Merge branches starting with "ready" into "main"
 git checkout main
-git merge ready*
+git merge $(git branch --list 'ready*')
 
-# Resolve any merge conflicts (if any)
+git branch -d $(git branch --list 'ready*')
 
-# Delete branches starting with "ready"
-git branch -d $(git branch -r | grep origin/ready | sed 's/origin\///')
-
-# Update branches starting with "update" from "main"
 git checkout main
-git pull
-git branch -r --list 'origin/update*' | grep -v '->' | sed 's/origin\///' | xargs -I {} sh -c 'git checkout {}; git merge origin/main'
+git pull origin main
+git checkout $(git branch --list 'update*')
+git merge main
 
-# Push the changes to the remote repository
-git push origin --all
